75698a0fbaba0e29fb0a5d08e23210ba3d1574d7
chobitsfan <chobits@itri.org.tw>
2021-02-25 17:05:09 +0800

    send mission or fence

--- wploader.py
+++ wploader.py
@@ -1,15 +1,19 @@
+import os
+os.environ["MAVLINK20"] = "1"
 from pymavlink import mavutil
 import csv, sys
 
-with open(sys.argv[2]) as csv_file:
+mission_type = int(sys.argv[3])
+file_name = sys.argv[2]
+with open(file_name) as csv_file:
     data = csv.reader(csv_file)
     waypoints = list(data)
 
-print(waypoints)
+#print(waypoints)
 master = mavutil.mavlink_connection(device=sys.argv[1], source_system=255)
 master.recv_match(type="HEARTBEAT", blocking=True)
-print("heartbeat recv")
-master.mav.mission_count_send(0, 0, len(waypoints))
+print("heartbeat recv", master.mavlink20())
+master.mav.mission_count_send(0, 0, len(waypoints), mission_type)
 while True:
     try:
         msg = master.recv_msg()
@@ -18,11 +22,13 @@ while True:
     if msg is not None:
         msg_type = msg.get_type()
         if msg_type == "MISSION_REQUEST_INT" or msg_type == "MISSION_REQUEST":
-            print("send", msg.seq)
+            print("recv", msg_type, msg.seq)
             wp = waypoints[msg.seq]
-            master.mav.mission_item_int_send(0, 0, msg.seq, 0, int(wp[3]), 0, 1, float(wp[4]), float(wp[5]), float(wp[6]), float(wp[7]), int(wp[8]), int(wp[9]), float(wp[10]))
+            master.mav.mission_item_int_send(0, 0, msg.seq, 0, int(wp[3]), 0, 1, float(wp[4]), float(wp[5]), float(wp[6]), float(wp[7]), int(wp[8]), int(wp[9]), float(wp[10]), mission_type)
         elif msg_type == "MISSION_ACK":
-            print("finished", msg.type)
+            print("recv ack", msg.type)
             break
+        elif msg_type == "STATUSTEXT":
+            print(msg.text)
         #else:
         #    print(msg_type)
